30951528df5d913f8984a3b11a6ff4bcc6e50c69
doomkinn-geek <doomkinn@yandex.ru>
2021-04-02 16:08:42 +0300

    task 3 done

---
+++ task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;//не стал делать беззнаковым, чтобы не усложнять программу обработкой исключений                       
+            Console.Write("Введите число: ");
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введено некорректное число");
+                return;                
+            }
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+            }
+        }
+    }
+}
